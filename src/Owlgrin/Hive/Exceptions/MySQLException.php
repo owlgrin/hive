54f46ecf5f59176215d23fa57f5d697e95ae6444
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-06-03 16:52:47 +0530

    added mysql exception to handle errors gracefully

---
+++ src/Owlgrin/Hive/Exceptions/MySQLException.php
@@ -0,0 +1,28 @@
+<?php namespace Owlgrin\Hive\Exceptions;
+
+class MySQlException extends Exception {
+
+	/**
+	 * Message
+	 */
+	const MESSAGE = 'hive::responses.message.bad_request';
+
+	/**
+	 * Code
+	 */
+	const CODE = 900;
+
+	/**
+	 * Constructor
+	 * @param mixed $messages
+	 * @param array $replacers
+	 */
+	public function __construct($exception, $messages = self::MESSAGE, $replacers = array())
+	{
+		switch($exception->getCode()) {
+			case '23000': throw new InvalidInputException($messages, $replacers, self::CODE);
+			default: throw new InternalException;
+		}
+		parent::__construct($messages, $replacers, self::CODE);
+	}
+}
