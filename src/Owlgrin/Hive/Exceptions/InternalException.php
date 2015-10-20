4266a582e71ce249007c12e6f1c865c6a7a9f2be
Sahil <sahilsarpal@gmail.com>
2015-10-20 14:37:32 +0530

    internal exception refactored

--- src/Owlgrin/Hive/Exceptions/InternalException.php
+++ src/Owlgrin/Hive/Exceptions/InternalException.php
@@ -13,14 +13,31 @@ class InternalException extends Exception {
 	const CODE = 500;
 
 	/**
+	 * Original Exception that this internal exception wraps.
+	 */
+	protected $original;
+
+	/**
 	 * Constructor
 	 * @param mixed $messages
 	 * @param array $replacers
 	 */
-	public function __construct($messages = null, $replacers = array())
+	public function __construct($messages = null, $replacers = array(), $original = null)
 	{
 		$messages = is_null($messages) ? static::MESSAGE : $messages;
 
+		$this->original = $original;
+
 		parent::__construct($messages, $replacers, static::CODE);
+	}
+
+	/**
+	 * Returns the original exception that this internal exception wraps.
+	 *
+	 * @return mixed The original exception that was thrown by the System.
+	 */
+	public function getOriginal()
+	{
+		return $this->original;
 	}
 }
